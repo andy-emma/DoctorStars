e69948a04a1ce354cb07def5db61f315f2606dbf
Emma <90576454+Emma-Alexia@users.noreply.github.com>
2021-12-13 17:12:31 -0600

    Delete botaoi directory

--- botaoi/Bot-devs/eval.js
+++
@@ -1,8 +0,0 @@
-module.exports = ({
-name: "reload",
-aliases: ["rd"],
-code: `$updateCommands 
-$description[$customEmoji[Yes_DoS] **Comandos actualizados**]
-$color[GREEN]
-$onlyForIDs[846794001050894360;756913496054169701;$customEmoji[No_DoS] **Solo mi creadores pueden usar este comando.**]`
-})
